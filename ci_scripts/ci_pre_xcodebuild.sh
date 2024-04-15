d68eec1a9386053a523157030d276c2992c15728
Mike Sanford <msanford1540@icloud.com>
2024-04-15 05:51:01 -0700

    ci script fix

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $CI_XCODEBUILD_ACTION = 'build' ]]; then
-    if which swiftlint >/dev/null
+    if which swiftlint >/dev/null; then
         (cd $PROJECT_DIR; swiftlint --strict)
     else
         echo "warning: swiftlint not installed!"
