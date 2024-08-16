fc21717deafdd7b0264811a471a588bff42c94a1
Mike Sanford <msanford1540@icloud.com>
2024-08-16 12:12:43 -0700

    Fix Unit Tests

--- TicTacToeEngineTests/TicTacToeEngineTests.swift
+++ TicTacToeEngineTests/TicTacToeEngineTests.swift
@@ -19,63 +19,118 @@ class TicTacToeEngineTests: XCTestCase {
 
     func testWinningLine2D() throws {
         XCTAssertEqual(
-            Set(GridGameboard.locations(for: GridWinningLine.horizontal(.top))),
-            Set([GridLocation(.top, .middle), .init(.top, .left), .init(.top, .right)])
+            Set(Grid3Gameboard.locations(for: Grid3WinningLine.horizontal(.top))),
+            Set([Grid3Location(.top, .middle), .init(.top, .left), .init(.top, .right)])
         )
         XCTAssertEqual(
-            Set(GridGameboard.locations(for: GridWinningLine.vertical(.left))),
-            Set([GridLocation(.top, .left), .init(.middle, .left), .init(.bottom, .left)])
+            Set(Grid3Gameboard.locations(for: Grid3WinningLine.vertical(.left))),
+            Set([Grid3Location(.top, .left), .init(.middle, .left), .init(.bottom, .left)])
         )
         XCTAssertEqual(
-            Set(GridGameboard.locations(for: GridWinningLine.diagonal(isBackslash: true))),
-            Set([GridLocation(.top, .left), .init(.middle, .middle), .init(.bottom, .right)])
+            Set(Grid3Gameboard.locations(for: Grid3WinningLine.diagonal(isBackslash: true))),
+            Set([Grid3Location(.top, .left), .init(.middle, .middle), .init(.bottom, .right)])
         )
         XCTAssertEqual(
-            Set(GridGameboard.locations(for: GridWinningLine.diagonal(isBackslash: false))),
-            Set([GridLocation(.top, .right), .init(.middle, .middle), .init(.bottom, .left)])
+            Set(Grid3Gameboard.locations(for: Grid3WinningLine.diagonal(isBackslash: false))),
+            Set([Grid3Location(.top, .right), .init(.middle, .middle), .init(.bottom, .left)])
         )
     }
 
-    func testWinningLine3D() throws {
+    func testStraightWinningLines3D() throws {
         XCTAssertEqual(
-            Set(CubeGameboard.locations(for: CubeWinningLine.horizontal(.top, .front))),
-            Set([CubeLocation(.top, .middle, .front), .init(.top, .left, .front), .init(.top, .right, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.horizontal(.top, .front))),
+            Set([
+                Cube4Location(.top, .left, .front),
+                .init(.top, .middleLeft, .front),
+                .init(.top, .middleRight, .front),
+                .init(.top, .right, .front)
+            ])
         )
         XCTAssertEqual(
-            Set(CubeGameboard.locations(for: CubeWinningLine.vertical(.left, .front))),
-            Set([CubeLocation(.top, .left, .front), .init(.middle, .left, .front), .init(.bottom, .left, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.vertical(.left, .front))),
+            Set([
+                Cube4Location(.top, .left, .front),
+                .init(.middleTop, .left, .front),
+                .init(.middleBottom, .left, .front),
+                .init(.bottom, .left, .front)])
+        )
+    }
+
+    func testDiagnolWinningLines3D() throws {
+        XCTAssertEqual(
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.zDiagonal(.front, isBackslash: true))),
+            Set([
+                Cube4Location(.top, .left, .front),
+                .init(.middleTop, .middleLeft, .front),
+                .init(.middleBottom, .middleRight, .front),
+                .init(.bottom, .right, .front)
+            ])
         )
         XCTAssertEqual(
-            Set(CubeGameboard.locations(for: CubeWinningLine.zDiagonal(.front, isBackslash: true))),
-            Set([CubeLocation(.top, .left, .front), .init(.middle, .middle, .front), .init(.bottom, .right, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.zDiagonal(.front, isBackslash: false))),
+            Set([
+                Cube4Location(.top, .right, .front),
+                .init(.middleTop, .middleRight, .front),
+                .init(.middleBottom, .middleLeft, .front),
+                .init(.bottom, .left, .front)
+            ])
         )
         XCTAssertEqual(
-            Set(CubeGameboard.locations(for: CubeWinningLine.zDiagonal(.front, isBackslash: false))),
-            Set([CubeLocation(.top, .right, .front), .init(.middle, .middle, .front), .init(.bottom, .left, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.yDiagonal(.top, isBackslash: true))),
+            Set([
+                Cube4Location(.top, .left, .back),
+                .init(.top, .middleLeft, .middleBack),
+                .init(.top, .middleRight, .middleFront),
+                .init(.top, .right, .front)
+            ])
         )
         XCTAssertEqual(
-            Set(CubeGameboard.locations(for: CubeWinningLine.yDiagonal(.top, isBackslash: true))),
-            Set([CubeLocation(.top, .left, .back), .init(.top, .middle, .middle), .init(.top, .right, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.yDiagonal(.top, isBackslash: false))),
+            Set([
+                Cube4Location(.top, .left, .front),
+                .init(.top, .middleLeft, .middleFront),
+                .init(.top, .middleRight, .middleBack),
+                .init(.top, .right, .back)
+            ])
         )
-        XCTAssertEqual(
-            Set(CubeGameboard.locations(for: CubeWinningLine.yDiagonal(.top, isBackslash: false))),
-            Set([CubeLocation(.top, .left, .front), .init(.top, .middle, .middle), .init(.top, .right, .back)])
-        )
+    }
+
+    func testCrossDiagnolWinningLines3D() throws {
         XCTAssertEqual( // top-left-front:bottom-right-back, isFront: true, isBackslash: true
-            Set(CubeGameboard.locations(for: CubeWinningLine.crossDiagonal(isFront: true, isBackslash: true))),
-            Set([CubeLocation(.top, .left, .front), .init(.middle, .middle, .middle), .init(.bottom, .right, .back)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.crossDiagonal(isFront: true, isBackslash: true))),
+            Set([
+                Cube4Location(.top, .left, .front),
+                .init(.middleTop, .middleLeft, .middleFront),
+                .init(.middleBottom, .middleRight, .middleBack),
+                .init(.bottom, .right, .back)
+            ])
         )
         XCTAssertEqual( // bottom-left-front:top-right-back, isFront: true, isBackslash: false
-            Set(CubeGameboard.locations(for: CubeWinningLine.crossDiagonal(isFront: true, isBackslash: false))),
-            Set([CubeLocation(.bottom, .left, .front), .init(.middle, .middle, .middle), .init(.top, .right, .back)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.crossDiagonal(isFront: true, isBackslash: false))),
+            Set([
+                Cube4Location(.bottom, .left, .front),
+                .init(.middleBottom, .middleLeft, .middleFront),
+                .init(.middleTop, .middleRight, .middleBack),
+                .init(.top, .right, .back)
+            ])
         )
         XCTAssertEqual( // top-left-back:bottom-right-front, isFront: false, isBackslash: true
-            Set(CubeGameboard.locations(for: CubeWinningLine.crossDiagonal(isFront: false, isBackslash: true))),
-            Set([CubeLocation(.top, .left, .back), .init(.middle, .middle, .middle), .init(.bottom, .right, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.crossDiagonal(isFront: false, isBackslash: true))),
+            Set([
+                Cube4Location(.top, .left, .back),
+                .init(.middleTop, .middleLeft, .middleBack),
+                .init(.middleBottom, .middleRight, .middleFront),
+                .init(.bottom, .right, .front)
+            ])
         )
         XCTAssertEqual( // bottom-left-back:top-right-front, isFront: false, isBackslash: false
-            Set(CubeGameboard.locations(for: CubeWinningLine.crossDiagonal(isFront: false, isBackslash: false))),
-            Set([CubeLocation(.bottom, .left, .back), .init(.middle, .middle, .middle), .init(.top, .right, .front)])
+            Set(Cube4Gameboard.locations(for: Cube4WinningLine.crossDiagonal(isFront: false, isBackslash: false))),
+            Set([
+                Cube4Location(.bottom, .left, .back),
+                .init(.middleBottom, .middleLeft, .middleBack),
+                .init(.middleTop, .middleRight, .middleFront),
+                .init(.top, .right, .front)
+            ])
         )
     }
 }
