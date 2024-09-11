6b0d9eb84cab97fcfbe8642b7aed419c4f2de7a1
Mike Sanford <msanford1540@icloud.com>
2024-09-11 10:42:18 -0700

    code cleanup

--- TicTacToeController/HomeMenuViewModel.swift
+++ TicTacToeController/HomeMenuViewModel.swift
@@ -20,7 +20,7 @@ public final class HomeMenuViewModel: ObservableObject, @unchecked Sendable {
     @Published public var gameSessionViewModel: GameSessionViewModel
     @Published public var botLevelName: String = .empty
 #if DEBUG
-    private let screenshot: Screenshot? = .homeMenu
+    private let screenshot: Screenshot?
 #endif
     public let grid3Controller = Grid3GameboardController()
     public let cube4Controller = Cube4GameboardController()
